08f83c396081b872cadbd32d6ebd5e0659086492
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-19 11:08:38 -0500

    commit
    add multipoint choise

--- table.py
+++ table.py
@@ -11,16 +11,16 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS QuestionsAnswer1
 cursor.execute('SELECT * FROM QuestionsAnswer1')
 
 cursor.execute('''INSERT INTO QuestionsAnswer1 VALUES
-                1, What is the tallest mountain in the world?", "A",
-                2, Who wrote the novel 'To Kill a Mockingbird'?, "B",
-                3, What is the chemical symbol for gold?", "A",
-                4, Who painted 'Starry Night'?", "B",
-                5, What is the smallest planet in our solar system?, "A",
-                6, Which city is known as the City of Love?, "A",
-                7, What is the fastest bird in the world?, "A",
-                8, Who discovered penicillin?, "B",
-                9, What is the capital of Australia?, "C",
-                10, Which planet is known as the Red Planet?, "A"
+                1, 'What is the tallest mountain in the world? A: Mount Everest B: K2 C: Mount Kilimanjaro', "A",
+                2, 'Who wrote the novel 'To Kill a Mockingbird? A: F. Scott Fitzgerald B: Harper Lee C: John Steinbeck', "B",
+                3, 'What is the chemical symbol for gold? A: Au B: Ag C: Fe', "A",
+                4, 'Who painted Starry Night? A: Pablo Picasso, B: Vincent van Gogh, C: Claude Monet', "B",
+                5, 'What is the smallest planet in our solar system? A: Mercury B: Pluto C: Earth', "A",
+                6, 'Which city is known as the City of Love? A: Paris B: Venice C: Rome', "A",
+                7, 'What is the fastest bird in the world? A: Peregrine Falcon B: Ostrich C: Hummingbird', "A",
+                8, 'Who discovered penicillin? A: Marie Curie B: Alexander Fleming C: Louis Pasteur', "B",
+                9, 'What is the capital of Australia? A: Sydney B: Melbourne C: Canberra', "C",
+                10,'Which planet is known as the Red Planet? A: Mars B: Venus C: Jupiter', "A"
                
                
                ''')
