46911cae351c8bba3f1c6b15f11b0afb6d0b6555
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-19 11:24:49 -0500

    commit
    added table and populate table

--- z1_createTable.py
+++ z1_createTable.py
@@ -8,3 +8,5 @@ cursor.execute('SELECT * FROM QuestionsAnswer1')
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS QuestionsAnswer1
                (id integer, questions text, answers text)''')
+
+print(cursor.fetchall())
