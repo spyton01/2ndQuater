5d0e9ca33b5324f4206a2433384d2a4d55e3caf8
Awirut Kongsila <160513323+spyton01@users.noreply.github.com>
2024-03-19 11:20:13 -0500

    commit
    attempt to create table

---
+++ z1_createTable.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+# create empty database
+connect = sqlite3.connect('QuestionsAnswer1.db')
+cursor = connect.cursor()
+
+cursor.execute('SELECT * FROM QuestionsAnswer1')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS QuestionsAnswer1
+               (id integer, questions text, answers text)''')
